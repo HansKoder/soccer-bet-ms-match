8e27e19d5f103ac4edccf60d659439c604c5b85d
hans.arias <hansarias74@gmail.com>
2022-12-12 01:36:48 -0500

    add validation if visiting team or local team not exists in database return an error message

--- src/main/java/com/hans/soccer/bet/msmatch/apis/MatchResource.java
+++ src/main/java/com/hans/soccer/bet/msmatch/apis/MatchResource.java
@@ -3,23 +3,27 @@ package com.hans.soccer.bet.msmatch.apis;
 import com.hans.soccer.bet.msmatch.dtos.MatchDto;
 import com.hans.soccer.bet.msmatch.dtos.TeamDto;
 import com.hans.soccer.bet.msmatch.entities.Match;
-import com.hans.soccer.bet.msmatch.repositories.MatchRepository;
 import com.hans.soccer.bet.msmatch.services.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/matches")
-public class MatchApi {
+public class MatchResource {
 
     @Autowired
     private MatchService service;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @GetMapping("/{matchId}")
     ResponseEntity<?> getMatch (@PathVariable Long matchId) {
@@ -50,10 +54,27 @@ public class MatchApi {
     ResponseEntity<?> saveMatch (@RequestBody MatchDto matchDto) {
         try {
 
+            Long visitingId = matchDto.getVisitingTeam().getId();
+            Long localId = matchDto.getLocalTeam().getId();
+
             // Validations
-            if (matchDto.getLocalTeam().getId().equals(matchDto.getVisitingTeam())) {
+            if (visitingId.equals(localId)) {
                 String msg = "Visiting team and local team should not same";
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(Collections.singletonMap("error", msg));
+            }
+
+            Optional<TeamDto> visiting = getTeamByWebClient(visitingId);
+            if (visiting.isEmpty()) {
+                String msg = "Visiting team not found";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(Collections.singletonMap("error", msg));
+            }
+
+            Optional<TeamDto> local = getTeamByWebClient(localId);
+            if (local.isEmpty()) {
+                String msg = "Local team not found";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body(Collections.singletonMap("error", msg));
             }
 
@@ -82,6 +103,23 @@ public class MatchApi {
     @GetMapping("/")
     ResponseEntity<?> getMatches () {
         return ResponseEntity.ok(service.getMatches());
+    }
+
+    private Optional<TeamDto> getTeamByWebClient (Long teamId) {
+        try {
+            TeamDto teamDto = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8095/api/football-teams/" + teamId )
+                    .retrieve()
+                    .bodyToMono(TeamDto.class)
+                    .block();
+
+            return Optional.of(teamDto);
+        } catch (Exception exception) {
+            System.out.println("Error " + exception.getMessage());
+
+            return Optional.empty();
+        }
     }
 
 }
